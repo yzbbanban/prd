971387685894f330e875b8ab9ad4123833a86651
yzbbanban <yzbbanban@live.com>
2019-01-16 17:20:52 +0800

    version->ip

--- src/main/java/com/pl/common/PageParamUtil.java
+++ src/main/java/com/pl/common/PageParamUtil.java
@@ -8,6 +8,15 @@ import com.pl.domain.dto.PageParamDTO;
 public class PageParamUtil {
 
     public static <T extends PageParamDTO> PageParamDTO setPageParam(T pageParam) {
+        if (pageParam == null) {
+            try {
+                pageParam = (T) PageParamDTO.class.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
         int pageNo = pageParam.getPageNo();
         int pageSize = pageParam.getPageSize();
 
