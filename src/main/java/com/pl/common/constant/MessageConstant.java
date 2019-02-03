a0564d81f097996085fc729078bc4abb46262add
yzbbanban <yzbbanban@live.com>
2019-02-03 17:44:49 +0800

    修改短信存储

---
+++ src/main/java/com/pl/common/constant/MessageConstant.java
@@ -0,0 +1,23 @@
+package com.pl.common.constant;
+
+/**
+ * @author wangban
+ * @date 14:37 2019/1/31
+ */
+public class MessageConstant {
+
+    /**
+     * 登录短信
+     */
+    public static final String SYSTEM_SMS_LOGIN_CODE_PHONE = "SYSTEM_SMS_LOGIN_CODE_PHONE:";
+
+    /**
+     * 更改密码短信
+     */
+    public static final String SYSTEM_SMS_MODIFY_CODE_PHONE = "SYSTEM_SMS_MODIFY_CODE_PHONE:";
+
+    /**
+     * 短信重复
+     */
+    public static final String SMS_REPEAT = "SMS_REPEAT:";
+}
--- src/main/java/com/pl/controller/backend/SmsController.java
+++ src/main/java/com/pl/controller/backend/SmsController.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpSession;
 
 import static com.pl.common.constant.MessageConstant.SMS_REPEAT;
 import static com.pl.common.constant.MessageConstant.SYSTEM_SMS_LOGIN_CODE_PHONE;
+import static com.pl.common.constant.MessageConstant.SYSTEM_SMS_MODIFY_CODE_PHONE;
 
 @RestController
 @RequestMapping("v1/manage/sms")
@@ -26,6 +27,23 @@ public class SmsController extends BaseApi {
     @ApiOperation(value = "发送获取token短信")
     @PostMapping(value = "token")
     public ResultJson<String> getTokenSms(SmsMessageDTO messageDTO) {
+        return getStringResultJson(messageDTO, SYSTEM_SMS_LOGIN_CODE_PHONE + messageDTO.getCountryCode() + messageDTO.getPhoneNumber());
+    }
+
+    @ApiOperation(value = "更改密码短信")
+    @PostMapping(value = "modify")
+    public ResultJson<String> getModifySms(SmsMessageDTO messageDTO) {
+        return getStringResultJson(messageDTO, SYSTEM_SMS_MODIFY_CODE_PHONE + messageDTO.getCountryCode() + messageDTO.getPhoneNumber());
+    }
+
+    /**
+     * 发送短信
+     *
+     * @param messageDTO 短信内容
+     * @param key        短信 key
+     * @return 发送结果
+     */
+    private ResultJson<String> getStringResultJson(SmsMessageDTO messageDTO, String key) {
         ISmsUtils sms = new SmsYpUtils();
         int codeLength = 4;
         ResultJson<String> resultJson = new ResultJson<>();
@@ -34,15 +52,15 @@ public class SmsController extends BaseApi {
         if (isCodeRepeat(resultJson, messageDTO.getCountryCode() + messageDTO.getPhoneNumber())) {
             return resultJson;
         }
-
         boolean result = sms.sendSms(messageDTO.getPhoneNumber(), messageDTO.getCountryCode(), code, SmsYpUtils.SMS_YP);
 
         if (result) {
-            localCache.setCache(SYSTEM_SMS_LOGIN_CODE_PHONE + messageDTO.getCountryCode() + messageDTO.getPhoneNumber(), code);
+            localCache.setCache(key, code);
             localCache.setCache(SMS_REPEAT + messageDTO.getCountryCode() + messageDTO.getPhoneNumber(), code);
             return ResultJson.createBySuccess();
         }
         return ResultJson.createByError();
     }
+
 
 }
