99ed6a61e72b0fbcb87ede9d3f899011968aed58
yzbbanban <yzbbanban@live.com>
2019-02-04 11:55:11 +0800

    修改短信存储

--- src/main/java/com/pl/common/constant/MessageConstant.java
+++ src/main/java/com/pl/common/constant/MessageConstant.java
@@ -12,11 +12,6 @@ public class MessageConstant {
     public static final String SYSTEM_SMS_LOGIN_CODE_PHONE = "SYSTEM_SMS_LOGIN_CODE_PHONE:";
 
     /**
-     * 更改密码短信
-     */
-    public static final String SYSTEM_SMS_MODIFY_CODE_PHONE = "SYSTEM_SMS_MODIFY_CODE_PHONE:";
-
-    /**
      * 短信重复
      */
     public static final String SMS_REPEAT = "SMS_REPEAT:";
