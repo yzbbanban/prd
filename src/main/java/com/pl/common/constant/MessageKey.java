451e4e96859d3421cdddbde6c25827f94858f1b5
yzbbanban <yzbbanban@live.com>
2019-02-18 08:13:26 +0800

    修改短信存储

---
+++ src/main/java/com/pl/common/constant/MessageKey.java
@@ -0,0 +1,9 @@
+package com.pl.common.constant;
+
+/**
+ * Created by brander on 2019/2/3
+ */
+public class MessageKey {
+    public static final String CODE_ERROR = "验证码错误";
+    public static final String CODE_REPEAT = "请不要频繁发送验证码";
+}
